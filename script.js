5517a3de939fd893ef656b20b51e0fe818677b78
Tanay Mayee <mayeetanay@gmail.com>
2024-07-30 16:48:54 -0700

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
 
 function navToggle() {
     // Toggles a different class for Hamburger button
     btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 }
 
 btn.addEventListener('click', navToggle)
